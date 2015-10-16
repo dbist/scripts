8adcdc4dde98c60c2d5cdf51d516c08a40f58498
Artem Ervits <generic13@gmail.com>
2015-10-16 16:37:23 -0400

    added min_free_kbytes property

--- administration/hbase.sh
+++ administration/hbase.sh
@@ -7,3 +7,7 @@ sysctl -w vm.swappiness=0
 # Increase this property to 1GB
 echo "vm.min_free_kbytes=1048576" >> /etc/sysctl.conf
 sysctl -w vm.min_free_kbytes=1048576
+
+# zone_reclaim_node
+echo "vm.zone_reclaim_mode=0" >> /etc/sysctl.conf
+sysctl -w vm.zone_reclaim_mode=0
