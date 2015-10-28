9086b3056a0aa802d02d1d9044832a15b44198ba
Artem Ervits <generic13@gmail.com>
2015-10-28 12:51:37 -0400

    new script to create and pre-split a table

---
+++ hbase/create_split_table.sh
@@ -0,0 +1,13 @@
+# script to create a table and pre-split
+# expects arguments 1: table name 2: COLUMNFAMILY 3: number of regions 4: algorithm (HexStringSplit or UniformSplit) 5: compression type (NONE) for no compression
+echo " expects arguments 1: table name 2: column family name (i.e. cf) 3: number of regions 4: algorithm (HexStringSplit or UniformSplit) 5: compression type (i.e. NONE)"
+$TABLE=$1
+$CF=$2
+$NUMREGIONS=$3
+$SPLITALGO=$4
+COMPRESSION=$5
+echo "disable '$TABLE'" | hbase shell
+echo "drop '$TABLE'" | hbase shell
+hbase org.apache.hadoop.hbase.util.RegionSplitter $TABLE $SPLITALGO -c $NUMREGIONS -f $CF
+sleep 3
+echo "alter '$TABLE', {NAME => 'cf', DATA_BLOCK_ENCODING => 'FAST_DIFF', COMPRESSION => '$COMPRESSION'}" | hbase shell
