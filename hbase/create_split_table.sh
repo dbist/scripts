e1ce74f3c3475c43b85186ad6051091f474189f5
Artem Ervits <generic13@gmail.com>
2015-11-12 13:43:47 -0500

    bug with params

--- hbase/create_split_table.sh
+++ hbase/create_split_table.sh
@@ -1,10 +1,10 @@
 # script to create a table and pre-split
 # expects arguments 1: table name 2: COLUMNFAMILY 3: number of regions 4: algorithm (HexStringSplit or UniformSplit) 5: compression type (NONE) for no compression
 echo " expects arguments 1: table name 2: column family name (i.e. cf) 3: number of regions 4: algorithm (HexStringSplit or UniformSplit) 5: compression type (i.e. NONE)"
-$TABLE=$1
-$CF=$2
-$NUMREGIONS=$3
-$SPLITALGO=$4
+TABLE=$1
+CF=$2
+NUMREGIONS=$3
+SPLITALGO=$4
 COMPRESSION=$5
 echo "disable '$TABLE'" | hbase shell
 echo "drop '$TABLE'" | hbase shell
