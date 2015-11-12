80b53a43d353d7470a2d19b1841743999b0da5bf
Artem Ervits <generic13@gmail.com>
2015-11-12 10:23:25 -0500

    added java scripts

---
+++ administration/jre8.sh
@@ -0,0 +1,8 @@
+# http://ask.xmodulo.com/set-java_home-environment-variable-linux.html
+
+# install java
+yum install install -y java-1.8.0-openjdk.x86_64
+
+echo "" ~/.bashrc
+echo "Set Java" >> ~/.bashrc
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
