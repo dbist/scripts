0d96c22eb41a258a46fa401b621bb0e5a42934df
Artem Ervits <generic13@gmail.com>
2015-11-12 10:21:44 -0500

    added java scripts

---
+++ administration/jdk7.sh
@@ -0,0 +1,8 @@
+# http://ask.xmodulo.com/set-java_home-environment-variable-linux.html
+
+# install java
+yum install install -y java-1.7.0-openjdk-devel.x86_64
+
+echo "" ~/.bashrc
+echo "Set Java" >> ~/.bashrc
+echo "export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))" >> ~/.bashrc
