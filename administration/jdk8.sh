f910909aad1e6e9eb8f55d95c972447c38e5d73e
Artem Ervits <generic13@gmail.com>
2015-11-12 11:38:22 -0500

    added java scripts

--- administration/jdk8.sh
+++ administration/jdk8.sh
@@ -6,3 +6,5 @@ yum install install -y java-1.8.0-openjdk-devel.x86_64
 echo "" ~/.bashrc
 echo "Set Java" >> ~/.bashrc
 echo "export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))" >> ~/.bashrc
+
+source ~/.bashrc
--- administration/jre7.sh
+++ administration/jre7.sh
@@ -6,3 +6,5 @@ yum install install -y java-1.7.0-openjdk.x86_64
 echo "" ~/.bashrc
 echo "Set Java" >> ~/.bashrc
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+
+source ~/.bashrc
