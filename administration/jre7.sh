1f2458a72c49ccad61dc6d4d5cea08eb6cd92bf1
Artem Ervits <generic13@gmail.com>
2015-11-12 11:42:17 -0500

    fix comment bug

--- administration/jre7.sh
+++ administration/jre7.sh
@@ -4,7 +4,7 @@
 yum install install -y java-1.7.0-openjdk.x86_64
 
 echo "" ~/.bashrc
-echo "Set Java" >> ~/.bashrc
+echo "# Set Java" >> ~/.bashrc
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 
 source ~/.bashrc
