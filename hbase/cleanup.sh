c26a24ab8d16ee35dafc636ad663afb4163438f0
Artem Ervits <generic13@gmail.com>
2015-11-17 18:40:41 -0500

    additional parameters

--- hbase/cleanup.sh
+++ hbase/cleanup.sh
@@ -1,2 +1,12 @@
 # erase everything in HBase, including zookeeper parent
+# Usage: hbase clean (--cleanZk|--cleanHdfs|--cleanAll)
+# Options:
+#        --cleanZk   cleans hbase related data from zookeeper.
+#        --cleanHdfs cleans hbase related data from hdfs.
+#        --cleanAll  cleans hbase related data from both zookeeper and hdfs.
+#
+#
+
 hbase clean --cleanAll
+
+
