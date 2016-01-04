c30f089100fe6bfedcdef214ee6412ddeab7814c
Artem Ervits <generic13@gmail.com>
2016-01-04 09:05:07 -0500

    added additional configurations to ntp for RHEL7

--- administration/rhel7/ntp.sh
+++ administration/rhel7/ntp.sh
@@ -1,8 +1,20 @@
 # Setup NTPD
 
+yum install -y ntp
 systemctl is-enabled ntpd
 systemctl enable ntpd
+
+# enable firewall rules for ntp
+firewall-cmd --add-service=ntp --permanent
+firewall-cmd --reload
 
 systemctl stop ntpd
 ntpdate pool.ntp.org
 systemctl start ntpd
+systemctl status ntpd
+
+echo "wait 30 sec for time to synchronize"
+sleep 30
+ntpq -p
+date -R
+
